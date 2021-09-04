6226583f81d619a1a73f57690e60ffc241148c35
Codewhitex <89073398+Codewhitex@users.noreply.github.com>
2021-09-04 15:02:20 +1000

    gulp.js
    DO NOT MERGE
    use Gulp, install Gulp plugins, configure the gulpfile.js and then use Gulp to automate the web development tasks.
    
    npm install -g gulp-cli@2.0.1 (global)
    
         npm install gulp@3.9.1 --save-dev (set location)
    npm install gulp-sass@3.1.0  browser-sync@2.23.6 --save-dev
    
    gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//    Load in all the Gulp plugins by including the following code in the Gulp file:
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync");
+//    Next, we will add the code for the SASS task, the Browser-Sync task and the default task as follows:
+gulp.task("sass", function () {
+  return gulp
+    .src("./css/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", function () {
+  gulp.watch("./css/*.scss", ["sass"]);
+});
+
+gulp.task("browser-sync", function () {
+  var files = ["./*.html", "./css/*.css", "./img/*.{png,jpg,gif}", "./js/*.js"];
+
+  browserSync.init(files, {
+    server: {
+      baseDir: "./",
+    },
+  });
+});
+
+// Default task
+gulp.task("default", ["browser-sync"], function () {
+  gulp.start("sass:watch");
+});
